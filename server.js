5be78a9f346006194d34b995cd8d196c1e998931
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-12-08 15:39:44 -0800

    threetestspassing

---
+++ server.js
@@ -0,0 +1,24 @@
+
+require('dotenv').config()
+
+const app = require('./lib/sql/app');
+const pool = require('./lib/utils/pool');
+
+
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
+});
+
+process.on('exit', () => {
+  console.log('Goodbye!');
+  pool.end();
+});
+
+
+
+
+
+
